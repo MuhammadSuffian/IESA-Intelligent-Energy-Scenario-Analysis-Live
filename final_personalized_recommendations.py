32e906960e8991e2aa803c5cd01276f4672c6459
Muhammad Suffian Tafoor <112380672+MuhammadSuffian@users.noreply.github.com>
2026-03-08 23:03:11 +0500

    Update final_personalized_recommendations.py

--- final_personalized_recommendations.py
+++ final_personalized_recommendations.py
@@ -491,23 +491,23 @@ def load_personalized_recommendations(logger):
         db_api_key=None
 
     SUPABASE_URL = "https://virfugeozdmixlglomoz.supabase.co"
-    supabase: Client = create_client(SUPABASE_URL, db_api_key)
+    # supabase: Client = create_client(SUPABASE_URL, db_api_key)
 
-    def fetch_data_with_sql():
-        try:
-            # Call the PostgreSQL function using rpc()
-            response = supabase.rpc('get_all_items').execute()
+    # def fetch_data_with_sql():
+    #     try:
+    #         # Call the PostgreSQL function using rpc()
+    #         response = supabase.rpc('get_all_items').execute()
 
-            # Handle the response
-            if response.data:
-                st.title("Data fetched successfully:")
-                for item in response.data:
-                     st.title(item)
-            else:
-                 st.title("No data returned or an error occurred.")
+    #         # Handle the response
+    #         if response.data:
+    #             st.title("Data fetched successfully:")
+    #             for item in response.data:
+    #                  st.title(item)
+    #         else:
+    #              st.title("No data returned or an error occurred.")
 
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    #     except Exception as e:
+    #         print(f"An error occurred: {e}")
 
     # fetch_data_with_sql()
 
